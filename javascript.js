ac0ace8acb0a488ecfca82fdab4dc85a13a150c5
creinken <reinkendigital@gmail.com>
2025-05-28 20:15:19 -0500

    Fix gap
    Fixxed gap between rows

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 function createBoard(gridWidth = 16) {
-    container = document.querySelector('.container');
+    let container = document.querySelector('.container');
+    let measure = 800 / gridWidth;
 
     for (let i = 0; i < gridWidth; i++) {
         let row = document.createElement("div");
@@ -7,6 +8,8 @@ function createBoard(gridWidth = 16) {
         for (let y = 0; y < gridWidth; y++) {
             let pixel = document.createElement("div");
             pixel.setAttribute("class", "pixel");
+            pixel.style.height = measure + 'px';
+            pixel.style.width = measure + 'px';
             row.appendChild(pixel);
         }
         container.appendChild(row);
